1508675bd1d8c214b4ec030c1ab9fc09a60403d9
Ramón Martínez Olvera <martinezo@inb.unam.mx>
2014-07-10 14:14:33 -0500

    Set up default route

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ PcrCloning::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'admin/users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
