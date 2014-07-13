759f3e108971fc84a9cf60797cc45001ce29ba97
Ramón Martínez Olvera <martinezo@inb.unam.mx>
2014-07-12 23:10:33 -0500

    Add new systen configuration parameter for deposit slip mail

--- app/models/admin/system_config.rb
+++ app/models/admin/system_config.rb
@@ -3,13 +3,14 @@ class Admin::SystemConfig
   require 'yaml'
 
   class << self
-    attr_accessor :internal_price, :external_price, :new_request_mails
+    attr_accessor :internal_price, :external_price, :new_request_mails, :new_deposit_slip_mail
 
     def get
       config = YAML.load_file('config/system_config.yml')
       self.internal_price = config['prices']['internal']
       self.external_price = config['prices']['external'] 
       self.new_request_mails = config['notifications']['new_request_mails']
+      self.new_deposit_slip_mail = config['notifications']['new_deposit_slip_mail']
     end
 
    def update(params)
@@ -17,6 +18,7 @@ class Admin::SystemConfig
       config['prices']['internal'] = params[:internal_price].to_i if params[:internal_price]
       config['prices']['external'] = params[:external_price].to_i if params[:external_price]
       config['notifications']['new_request_mails'] = params[:new_request_mails] if params[:new_request_mails] 
+      config['notifications']['new_deposit_slip_mail'] = params[:new_deposit_slip_mail] if params[:new_deposit_slip_mail] 
       File.open('config/system_config.yml','w') {|f| f.write config.to_yaml}
    end
   end
