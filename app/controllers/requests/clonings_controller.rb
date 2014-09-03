fc7c10199b617d2d4fcb6446fc60a78671154fa1
Roberto Martínez <rmartinez1006@prodigy.net.mx>
2014-09-02 23:22:55 -0500

    full implementation xlsx

--- app/controllers/requests/clonings_controller.rb
+++ app/controllers/requests/clonings_controller.rb
@@ -143,6 +143,7 @@ class Requests::CloningsController < ApplicationController
     package.serialize("public/xlsx/pcr_clonings_requests.xlsx")
   end
 
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_requests_cloning
